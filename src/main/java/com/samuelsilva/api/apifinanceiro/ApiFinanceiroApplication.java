8d1ec3eb4b8702c3e7d3fe8815eb97cb4e7eff28
samuel silva <samuel@sotechsaude.com.br>
2017-07-31 15:06:44 -0300

    Iniciando projeto

---
+++ src/main/java/com/samuelsilva/api/apifinanceiro/ApiFinanceiroApplication.java
@@ -0,0 +1,12 @@
+package com.samuelsilva.api.apifinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinanceiroApplication.class, args);
+	}
+}
