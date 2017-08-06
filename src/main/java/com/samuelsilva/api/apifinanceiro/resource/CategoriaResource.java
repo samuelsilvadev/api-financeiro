ccae7a61e795ad69acd52c15f3647d0f76f32927
samuel <samuelsilvawb@gmail.com>
2017-08-06 11:03:40 -0300

    Criando método para salvar categorias

--- src/main/java/com/samuelsilva/api/apifinanceiro/resource/CategoriaResource.java
+++ src/main/java/com/samuelsilva/api/apifinanceiro/resource/CategoriaResource.java
@@ -1,13 +1,19 @@
 package com.samuelsilva.api.apifinanceiro.resource;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.samuelsilva.api.apifinanceiro.model.Categoria;
 import com.samuelsilva.api.apifinanceiro.repository.CategoriaRepository;
@@ -31,4 +37,16 @@ public class CategoriaResource {
 		 return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.noContent().build();  
 	}
 	
+	@PostMapping
+	public ResponseEntity<?> save(@RequestBody Categoria categoria, HttpServletResponse response){
+		Categoria newCat = categoriaRepository.save(categoria);
+
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequestUri()
+					.path("/{id}")
+					.buildAndExpand(newCat.getCodigo()).toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+		return ResponseEntity.created(uri).body(newCat);		
+	}	
 }
