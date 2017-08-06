0f835c7f128312d148eb88e8cc1b3d4c68b3c6ff
samuel <samuelsilvawb@gmail.com>
2017-08-06 10:18:42 -0300

    Criando recurso para mostrar todas as categorias

--- src/main/java/com/samuelsilva/api/apifinanceiro/resource/CategoriaResource.java
+++ src/main/java/com/samuelsilva/api/apifinanceiro/resource/CategoriaResource.java
@@ -1,5 +1,27 @@
 package com.samuelsilva.api.apifinanceiro.resource;
 
-public class CategoriaResource {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.samuelsilva.api.apifinanceiro.model.Categoria;
+import com.samuelsilva.api.apifinanceiro.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+		
+	@GetMapping
+	public ResponseEntity<?> findAll(){
+		 List<Categoria> listCat = categoriaRepository.findAll();
+		 return !listCat.isEmpty() ? ResponseEntity.ok(listCat) : ResponseEntity.noContent().build();  
+	}
+	
 }
