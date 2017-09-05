6b2376acdacd2faaa1d0365dc20b4337495b9601
samuel <samuelsilvawb@gmail.com>
2017-09-04 22:06:21 -0300

    Criando método para verificar se pessoa esta desabilitada

--- src/main/java/com/samuelsilva/api/apifinanceiro/model/Pessoa.java
+++ src/main/java/com/samuelsilva/api/apifinanceiro/model/Pessoa.java
@@ -6,8 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -36,4 +39,9 @@ public class Pessoa {
 	@Getter @Setter
 	private Endereco endereco;
 	
+	@JsonIgnore
+	@Transient
+	public boolean isInativo(){
+		return !this.ativo;
+	}	
 }
